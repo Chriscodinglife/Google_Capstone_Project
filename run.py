8be150b1c8162f27959138e9432bbff4bc661154
Chriscodinglife <chrisdotorellana@gmail.com>
2022-06-22 19:24:14 -0400

    Added a new run.py file for the coursera captone project

---
+++ run.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''
+ ______     __  __     ______     __     ______    
+/\  ___\   /\ \_\ \   /\  == \   /\ \   /\  ___\   
+\ \ \____  \ \  __ \  \ \  __<   \ \ \  \ \___  \  
+ \ \_____\  \ \_\ \_\  \ \_\ \_\  \ \_\  \/\_____\ 
+  \/_____/   \/_/\/_/   \/_/ /_/   \/_/   \/_____/ 
+                                                   
+June 2022
+
+This script will grab two directories, one of text files, and another of images
+and create a json post request of the information available from the files.
+
+'''
+
