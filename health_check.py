3320da5971f93ba1f12f07a5c295d6efdcfc8871
Chriscodinglife <chrisdotorellana@gmail.com>
2022-06-23 11:36:52 -0400

    Started to create the health_check.py script

---
+++ health_check.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''
+ ______     __  __     ______     __     ______    
+/\  ___\   /\ \_\ \   /\  == \   /\ \   /\  ___\   
+\ \ \____  \ \  __ \  \ \  __<   \ \ \  \ \___  \  
+ \ \_____\  \ \_\ \_\  \ \_\ \_\  \ \_\  \/\_____\ 
+  \/_____/   \/_/\/_/   \/_/ /_/   \/_/   \/_____/ 
+                                                   
+June 2022
+
+This script is designed to raise an alert if there is an issue with the current
+machine. If there is an issue, it will send an email so that someone can review
+the status of the device
+
+'''
