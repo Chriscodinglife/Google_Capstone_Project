cf1833f744c2277eadef81006f4b66b22685e074
Chriscodinglife <chrisdotorellana@gmail.com>
2022-06-23 11:28:28 -0400

    Added some comments to clear up what is going on and also added the emails.generate_email and emails.send_email functions

--- report_email.py
+++ report_email.py
@@ -16,18 +16,22 @@ via an email
 
 ## IMPORT
 import reports
+import emails
 
 
 ## FUNCTIONS
 
 def main():
 
+    # Get Today's date
     today = reports.todays_date()
     title = "Processed Update on {}".format(today)
 
+    # Get the list of text files
     text_files = "~/supplier-data/descriptions"
     text_list = reports.get_text_dict(text_files)
 
+    # Create the PDF Body
     summary_pdf_body = ""
     for dict in text_list:
         summary_pdf_body += "<br/>"
@@ -37,8 +41,20 @@ def main():
             summary_pdf_body += value[0] + ": " + value[1]
         summary_pdf_body += "<br/>"
 
+    # Create the PDF
     report_file_name = "/tmp/processed.pdf"
     reports.generate_report(report_file_name, title, summary_pdf_body)
+
+    # Prepare the email to be sent
+    sender = "automation@example.com"
+    user = "<enter_user_here>"
+    receiver = "{}@example.com".format(user)
+    subject = "Upload Completed - Online Fruit Store"
+    email_body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+
+    # Send email
+    email = emails.generate_email(sender, receiver, subject, email_body, report_file_name)
+    emails.send_email(email)
 
 
 ## RUN
