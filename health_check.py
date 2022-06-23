e5c13bcf24022c327a6956b7227e6e2d2f22e347
Chriscodinglife <chrisdotorellana@gmail.com>
2022-06-23 11:55:03 -0400

    Added the check_disk_full() and the check_root_full() functions and started building out main()

--- health_check.py
+++ health_check.py
@@ -14,3 +14,42 @@ machine. If there is an issue, it will send an email so that someone can review
 the status of the device
 
 '''
+
+## IMPORT
+import os
+import sys
+import shutil
+import psutil
+import emails
+
+
+## FUNCTIONS
+def check_disk_full(disk, min_percent):
+
+    """Returns True if there isnt enough disk space, False Otherwise"""
+    du = shutil.disk_usage(disk)
+
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+
+    if percent_free < min_percent:
+        return True
+
+    return False
+
+
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise"""
+    return check_disk_full(disk="/", min_percent=20)
+
+
+def main():
+
+    checks =[
+        (check_root_full, "Root partition full"),
+    ]
+
+    for check, msg in checks:
+        if check():
+            print(msg)
+
